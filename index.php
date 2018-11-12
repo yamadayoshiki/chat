ecf15151941ab1ddf4119e42fd431fbea7a26bb1
yoshiki <raikinex430@gmail.com>
2018-11-12 15:05:15 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+</head>
+<bidy>
+<h1>ログイン</h1>
+<form action = "chat.php">
+	<input type = "text" name = "uname">
+	<button>ログイン</button>
+</form> 
+</body>
+</html>
